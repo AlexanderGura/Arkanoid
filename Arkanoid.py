039825e58b164df0684bab8938e3b2e648823738
Alex Gura <gura_alex@list.ru>
2023-09-07 11:59:24 +0700

    Add boll and platform rects.

--- Arkanoid.py
+++ Arkanoid.py
@@ -1,3 +1,19 @@
-import pygame
+import pygame, sys
 
-print('hello pygame')
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+screen_rect = screen.get_rect()
+
+boll = pygame.Rect(0, 0, 40, 40)
+platform = pygame.Rect(0, 0, 150, 20)
+platform.midbottom = screen_rect.midbottom
+
+while True:
+    screen.fill((255, 255, 255), boll)
+    screen.fill((255, 255, 255), platform)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
