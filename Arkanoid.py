58b43bafc1954ae2712230fa7b99a99bb2f37b54
Alex Gura <gura_alex@list.ru>
2023-09-07 12:15:37 +0700

    Add comments and consts.

--- Arkanoid.py
+++ Arkanoid.py
@@ -1,19 +1,34 @@
 import pygame, sys
 
 pygame.init()
-screen = pygame.display.set_mode((600, 600))
+
+# Константы.
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 600
+BOLL_SIZE = 40
+PLATFORM_WIDTH = 150
+PLATFORM_HEIGHT = 20
+
+# Создание экрана и получение поверхности и квадрата окна.
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 screen_rect = screen.get_rect()
 
-boll = pygame.Rect(0, 0, 40, 40)
-platform = pygame.Rect(0, 0, 150, 20)
+# Создание объектов Rect для представления мяча и платформы.
+boll = pygame.Rect(0, 0, BOLL_SIZE, BOLL_SIZE)
+platform = pygame.Rect(0, 0, PLATFORM_WIDTH, PLATFORM_HEIGHT)
 platform.midbottom = screen_rect.midbottom
 
+# Основной цикл игры.
 while True:
-    screen.fill((255, 255, 255), boll)
-    screen.fill((255, 255, 255), platform)
+    # Цикл для проверки событий клавиатуры, мыши, окна.ы
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    # Заливка квадратов мяча и платформы белым цветом.
+    screen.fill((255, 255, 255), boll)
+    screen.fill((255, 255, 255), platform)
+
+    # Обновление экрана после каждого прохода цикла.
     pygame.display.update()
